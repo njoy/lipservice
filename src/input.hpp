24091188e7a5a0374db0d779a55290675900dbc4
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-08-27 16:14:31 -0600

    Adding source code from other project.

---
+++ src/input.hpp
@@ -0,0 +1,40 @@
+namespace njoy {
+namespace njoy21 {
+namespace input {
+
+template< typename Char >
+using iRecordStream =
+  utility::stream::basic_RecordOrientedStream< std::basic_istream, Char >;
+
+
+#include "input/Card.hpp"
+#include "input/Label.hpp"
+#include "input/argument.hpp"
+
+#include "input/MODER.hpp"
+#include "input/RECONR.hpp"
+#include "input/BROADR.hpp"
+#include "input/PURR.hpp"
+#include "input/UNRESR.hpp"
+#include "input/ACER.hpp"
+#include "input/GASPR.hpp"
+#include "input/HEATR.hpp"
+#include "input/GROUPR.hpp"
+#include "input/VIEWR.hpp"
+#include "input/MIXR.hpp"
+#include "input/DTFR.hpp"
+#include "input/THERMR.hpp"
+#include "input/ERRORR.hpp"
+#include "input/LEAPR.hpp"
+#include "input/RESXSR.hpp"
+#include "input/MATXSR.hpp"
+#include "input/GAMINR.hpp"
+#include "input/PLOTR.hpp"
+#include "input/COVR.hpp"
+#include "input/WIMSR.hpp"
+#include "input/POWR.hpp"
+#include "input/CCCCR.hpp"
+
+} // namespace input
+} // namespace njoy21
+} // namespace njoy
---
+++ src/input/argument/common.hpp
@@ -0,0 +1,8 @@
+namespace common {
+
+#include "njoy21/input/argument/common/Nin.hpp"
+#include "njoy21/input/argument/common/Nout.hpp"
+#include "njoy21/input/argument/common/Matd.hpp"
+#include "njoy21/input/argument/common/Tempd.hpp"
+
+}
