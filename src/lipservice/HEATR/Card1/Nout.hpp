ffbeb06b7f6e4a5085ce092e099fc7cc7c8b1ee7
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-05 04:31:12 -0600

    Fixing problem with verification of HEATR/Card1/Nout.

--- src/lipservice/HEATR/Card1/Nout.hpp
+++ src/lipservice/HEATR/Card1/Nout.hpp
@@ -12,7 +12,7 @@ struct Nout : argument::common::Nout {
   static bool verify( const Value_t v,
 		      const Argument< Nendf >& nendf,
 		      const Argument< Nin >& nin ){
-    return argument::common::Nout::verify( v )
+    return argument::common::Nout::verify( v, nin )
       && ( std::abs(v) != std::abs( nendf.value ) )
       && ( std::abs(v) != std::abs( nin.value   ) );
   }
