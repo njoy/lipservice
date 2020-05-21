0d1f375b65b878f22105d07d56b77ca230321b58
Nathan Gibson <ngibson@lanl.gov>
2020-05-21 15:01:52 -0600

    Added GIT_SHALLOW

--- cmake/lipservice_dependencies.cmake
+++ cmake/lipservice_dependencies.cmake
@@ -13,26 +13,31 @@ include( FetchContent )
 FetchContent_Declare( ENDFtk
     GIT_REPOSITORY  http://github.com/njoy/ENDFtk
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  http://github.com/njoy/catch-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( hana-adapter
     GIT_REPOSITORY  http://github.com/njoy/hana-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( json-adapter
     GIT_REPOSITORY  http://github.com/njoy/json-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( utility
     GIT_REPOSITORY  http://github.com/njoy/utility
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 
