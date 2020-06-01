5fa1e165c8a9d076250dbffdf32d3916d05d5e1f
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-06-01 09:19:42 -0600

    Resolving #17

--- src/lipservice/ACER/Card1/Ndir/test/Ndir.test.cpp
+++ src/lipservice/ACER/Card1/Ndir/test/Ndir.test.cpp
@@ -20,8 +20,9 @@ SCENARIO( "ndir output values",
 
   GIVEN( "valid ndir input values" ){
     WHEN( "the nendf and npend values are not the same as the ndir values" ){
-      THEN( "the returned class has the correct tape value" ){
-        for( auto ndir : {-20, 20, 50, 99, -99} ){
+      for( auto ndir : {-20, 20, 0, 50, 99, -99} ){
+        THEN( "the returned class has the correct tape value: " 
+          + std::to_string( ndir) ){
           iRecordStream<char> issNdir( 
               std::istringstream( std::to_string( ndir ) ) );
           REQUIRE( ndir == argument::extract< ACER::Card1::Ndir >(
