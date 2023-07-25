6e037e5960d1049000a3471540908f5317429e24
Wim Haeck <whaeck@gmail.com>
2023-07-25 13:36:49 -0600

    ielas may be 0, 1 or 2

--- src/lipservice/ACER/Card9/Ielas.hpp
+++ src/lipservice/ACER/Card9/Ielas.hpp
@@ -3,16 +3,17 @@ struct Ielas {
   static std::string name(){ return "ielas"; }
 
   static std::string description() {
-    return 
+    return
         "The ielas argument specifies whether the elastic scattering is \n"
-        "coherent or incoherent.\n"
+        "coherent, incoherent or both.\n"
         "\n"
-        "A value of 0 specifies coherent scattering while a value of 1 \n"
-        "specifies incoherent scattering. The default value is 0.";
+        "A value of 0 specifies coherent scattering, a value of 1 \n"
+        "specifies incoherent scattering and a value of 2 specifies both.\n"
+        "The default value is 0.";
   }
 
   static Value_t defaultValue(){ return 0; }
   static bool verify( const Value_t& ielas ){
-    return ( ielas == 0 ) or ( ielas == 1);
+    return ( ielas == 0 ) or ( ielas == 1) or ( ielas == 2);
   }
 };
